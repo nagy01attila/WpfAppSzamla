ba16f6b03158322b74b4db5ae3f5df0479528d30
nagy01attila <fatikajoker@gmail.com>
2023-12-11 19:30:03 +0100

    Add project files.

---
+++ WpfAppSzamla/Tetel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppSzamla
+{
+    public class Tetel
+    {
+        String termekNev;
+        int egysegAr;
+        String egyseg;
+        double mennyiseg;
+
+        public Tetel(string termekNev, int egysegAr, string egyseg, double mennyiseg)
+        {
+            this.termekNev = termekNev;
+            this.egysegAr = egysegAr;
+            this.egyseg = egyseg;
+            this.mennyiseg = mennyiseg;
+        }
+
+        public string TermekNev { get; set;  }
+        public int EgysegAr { get; set; }
+        public string Egyseg { get; set; }
+        public double Mennyiseg { get; set; }
+    }
+}
